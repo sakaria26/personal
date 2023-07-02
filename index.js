7dd7cd9825c08c7672aaf75daebd074bbacec16f
Sakaria Nghivafe <snghivafe@gmail.com>
2023-07-03 00:54:18 +0200

    Added Content Div Display Function

---
+++ index.js
@@ -0,0 +1,42 @@
+// function to remove focused class from call nav links
+const removeFocus = () => {
+    let navLinks = document.getElementsByClassName("nav-link");
+    for (let i = 0; i < navLinks.length; i++) {
+        navLinks[i].className = navLinks[i].className.replace("focused", "");
+    }
+};
+
+// function to add and remove visible and hidden classes
+const changeVisibility = (targetID) => {
+    let elementToShow = document.getElementById(targetID);
+    let current = document.getElementsByClassName("visible");
+
+    current[0].className = current[0].className.replace("visible", "hidden");
+    elementToShow.className = elementToShow.className.replace(
+        "hidden",
+        "visible"
+    );
+};
+
+const showHide = (targetID, navID) => {
+    let nav = document.getElementById(navID);
+
+    // remove focused class from all nav links
+    removeFocus();
+
+    // add focused class to current nav link
+    nav.className += " focused";
+
+    // change visibility of elements
+    changeVisibility(targetID);
+};
+
+const showContactMe = () => {
+    // remove focused class from all nav links
+    removeFocus();
+    // change visibility of elements
+    changeVisibility("contact-me");
+    // change the button text
+    let button = document.getElementById("btn-text");
+    button.innerHTML = "Back";
+};
